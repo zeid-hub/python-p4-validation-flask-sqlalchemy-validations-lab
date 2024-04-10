90ae9147ef4ebb0fd0e1e3dcab7a3b5f5321a3bd
zeid <zeidhassan4@gmail.com>
2024-04-10 22:23:07 +0300

    Done

--- server/models.py
+++ server/models.py
@@ -12,6 +12,22 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Author must have a name')
+
+        existing_author = Author.query.filter_by(name=name).first()
+        if existing_author and existing_author.id != self.id:
+            raise ValueError('Another author with the same name already exists')
+
+        return name
+    
+    @validates('phone_number')
+    def validates_phone_number(self, key, phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError('Phone number must be 10 digits long')
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,7 +44,30 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Content must be at least 250 characters long')
+        return content
 
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError('Summary must be a maximum of 250 characters')
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category.lower() not in ['fiction', 'non-fiction']:
+            raise ValueError('Category must be either Fiction or Non-Fiction')
+        return category
+
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_keywords = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(keyword in title for keyword in clickbait_keywords):
+            raise ValueError('Title must contain one of the following: "Won\'t Believe", "Secret", "Top", "Guess"')
+        return title
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
